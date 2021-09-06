54600a097e26a8964e0ad533c235bea3413a1d92
Kenny Sierra <kennysierra@Kennys-MacBook-Pro.local>
2021-09-06 17:54:33 -0600

    ffirst commit

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = "4ee36056dbf94deea2d9f77821d9621a";
+const Token = "0064ee36056dbf94deea2d9f77821d9621aIADZrfkQrX/u5wBKE6TrjVtA30s2Xs9hAatIs+k8CdL7NfMtSZcAAAAAEABSSZ5eiPc2YQEAAQCI9zZh";
